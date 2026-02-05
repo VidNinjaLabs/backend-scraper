74dc1a93cee02d9e50680b4bebb0b9b2ed66d6b5
rev9dev-netizen <rev9dev@gmail.com>
2026-02-05 23:31:00 +0530

    Allow dynamic PORT configuration

--- server.ts
+++ server.ts
@@ -284,7 +284,8 @@ app.post(
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000, host: "0.0.0.0" });
+    const port = parseInt(process.env.PORT || "3000");
+    await fastify.listen({ port, host: "0.0.0.0" });
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
